dfae6c9af2a7efea4c6c27cb6c526c58957124e8
prembhaskal <pbhaskal@gmail.com>
2017-02-24 19:45:32 +0530

    program using constants.

---
+++ cpractice/basicc/celsiusConstants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+int main() {
+    float fahr;
+
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) {
+        printf("%3.0f %6.2f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    }
+
+    return 0;
+}
