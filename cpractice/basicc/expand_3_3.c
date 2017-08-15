caf526b01ae1f990b22bd45bbf383e3cf2432691
prembhaskal <pbhaskal@gmail.com>
2017-08-16 00:17:15 +0530

    expand_3_3.c -- little ugly but handles most cases.

---
+++ cpractice/basicc/expand_3_3.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define LIMIT 40
+
+void expand(char [], char []);
+int readLineSize(char [], int);
+
+int main() {
+    char s1[LIMIT], s2[LIMIT];
+    readLineSize(s1, LIMIT);
+    expand(s1, s2);
+
+    printf("string %s is expanded to %s", s1, s2);
+}
+
+void expand(char s1[], char s2[]) {
+    int idx, idy, lastch;
+
+    int hypenbw = 0; // no hypen in between at start.
+
+
+    int s1len = strlen(s1);
+
+    // read leading hypens
+    for (idx = 0, idy = 0; s1[idx] == '-'; ++idx, ++idy)
+        s2[idy] = '-';
+
+    for (; idx < s1len; ++idx) {
+        if (s1[idx] == '-') {
+            if (hypenbw) { // read continuous non-leading hypens.
+                s2[idy++] = '-';
+            }
+            hypenbw = 1;
+        }
+        else if (hypenbw) {
+            // read lastch, note lastch is already captured.
+            ++lastch;
+            for (; lastch <= s1[idx]; ++idy) {
+                s2[idy] = lastch;
+                lastch++;
+            }
+            lastch = s1[idx]; // last element read.
+            hypenbw = 0;
+        }
+        else {
+            s2[idy++] = s1[idx];
+            lastch = s1[idx];
+            hypenbw = 0;
+        }
+    }
+
+    // last element was hypen
+    s2[idy++] = '-';
+
+    s2[idy] = '\0';
+}
+
+int readLineSize(char s[], int lim) {
+    int ch, idx;
+    for (idx = 0 ; ((ch = getchar()) != EOF) && idx < lim; ++idx) {
+        s[idx] = ch;
+    }
+
+    s[idx] = '\0';
+    return idx;
+}
+
---
+++ cpractice/basicc/reverse_inplace.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#define LIMIT 20
+
+void reverse_inplace(char []);
+int readLineSize(char s[], int);
+
+int main() {
+    char s[LIMIT];
+    readLineSize(s, LIMIT);
+    printf("string is %s ", s);
+    reverse_inplace(s);
+
+    printf(" and reverse of it is %s \n", s);
+}
+
+void reverse_inplace(char s[]) {
+    int i, j, ch;
+
+    for(i= 0,j = strlen(s) - 1 ; i < j; ++i, --j) {
+        ch = s[i];
+        s[i] = s[j];
+        s[j] = ch;
+    }
+
+}
+
+int readLineSize(char s[], int lim) {
+    int ch, idx;
+    for (idx = 0 ; ((ch = getchar()) != EOF) && idx < lim; ++idx) {
+        s[idx] = ch;
+    }
+
+    s[idx] = '\0';
+    return idx;
+}
