17437c1468672aa2c01fd8028b6dca7bc0f151f0
prembhaskal <pbhaskal@gmail.com>
2017-08-15 16:36:02 +0530

    atoi version 1. test.txt to read input from.

--- cpractice/README.txt
+++ cpractice/README.txt
@@ -15,3 +15,5 @@ explore variable
 # include maths functions in linux. like <math.h>, else you'll get error like  undefined reference to 'sqrt'
 gcc -lm
 
+# windows - stdin as input
+echo "123" | cprog.exe
---
+++ cpractice/basicc/atoi1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define LIMIT 20
+
+int atoi(char[]);
+int readLineAndSize(char [], int);
+
+int main() {
+    char s[LIMIT];
+    int len = readLineAndSize(s, LIMIT);
+
+    int num = atoi(s);
+
+    printf("string %s of len %d and converted to number is %d", s, len, num);
+}
+
+/*
+skip whitespaces if any
+read sign if any
+read integer part and convert it.
+*/
+int atoi(char s[]) {
+    int sign, n , idx;
+
+    for (idx = 0; isspace(s[idx]); ++idx)
+        ;
+
+    sign = (s[idx] == '-') ? -1 : 1;
+
+    if (s[idx] == '-' || s[idx] == '+')
+        ++idx;
+
+    for (n = 0; isdigit(s[idx]); ++idx) {
+        n = n*10 + (s[idx] - '0');
+    }
+
+    return sign * n;
+}
+
+int readLineAndSize(char s[], int limit) {
+    int ch, idx;
+
+    for (idx = 0; (ch = getchar()) != EOF && idx < limit; ++idx) {
+        s[idx] = ch;
+    }
+
+    s[idx] = '\0';
+    return idx;
+}
