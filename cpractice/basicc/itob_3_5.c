d92a7d4f5c94d5b6de6077936118aaf2d2390dab
pbhaskal <pbhaskal@gmail.com>
2017-10-27 08:25:09 +0530

    completed the conversion method, base is hard coded inside the  main method for now.

--- cpractice/basicc/itob_3_5.c
+++ cpractice/basicc/itob_3_5.c
@@ -1,23 +1,74 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 #define LIMIT 20
 
-int readSizeAndLine(char[] , int);
+int itob(int, char[], int);
+int convertToDecimalInt(char[]);
+int readSizeAndLine(char[], int);
+char convertToAscii(int);
+void reverseStr(char[]);
 
 /*
 read integer n and convert to base b character representation into string s.
 */
 int main() {
 
+  int base = 8;
+
   char s[LIMIT];
   int lineSize = readSizeAndLine(s, LIMIT);
   printf("read line of size %d and content %s\n", lineSize, s);
+
+  int readNum = convertToDecimalInt(s);
+  printf("converted number is %d\n", readNum);
+
+  char convertedStr[LIMIT];
+  itob(readNum, convertedStr, base);
+
+  printf("string after conversion to base %d is %s", base, convertedStr);
+  printf("\n");
+
   return 0;
 }
 
 int itob(int n, char s[], int base) {
+  int idx = 0;
+  int rem;
 
+  while (n > 0) {
+    rem = n % base;
+    s[idx++] = convertToAscii(rem);
+    n /= base;
+  }
+  s[idx] = '\0';
+
+  reverseStr(s);
+
+  return 0;
+}
+
+void reverseStr(char s[]) {
+  int i, j;
+  int tmp;
+  for (i = 0, j = strlen(s) - 1;i < j ; ++i, --j) {
+    tmp = s[i];
+    s[i] = s[j];
+    s[j]= tmp;
+  }
+}
+
+char convertToAscii(int n) {
+  if (n >= 0 && n < 10) {
+    return '0' + n;
+  }
+
+  if (n > 10 && n < 16) {
+    return 'A' + n - 10;
+  }
+
+  return 'X';
 }
 
 int convertToDecimalInt(char s[]) {
