d7e4627cae1570516d20adb01d179856db25ce30
pbhaskal <pbhaskal@gmail.com>
2017-11-03 08:24:16 +0530

    right most matching program complete. corrected the readline function to read only at most limit-1 chars + 1 last place for NULL character.

--- cpractice/basicc/strindex_4_1.c
+++ cpractice/basicc/strindex_4_1.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 #define LIMIT 20
 
+int getlineAndSize(char [], int);
+int strIndex(char [], char []);
+
 int main() {
   char t[] = "prem";
+  char s[LIMIT];
+  getlineAndSize(s, LIMIT);
+  printf("read line is %s\n", s);
+  printf("read line length is %ld\n", strlen(s));
+
+  int matchIdx = strIndex(s, t);
+  printf("pattern rightmost occurence found at %d\n", matchIdx);
 }
 
 int getlineAndSize(char s[], int limit) {
-  
+  int idx,ch;
+
+  for (idx = 0; idx < limit-1 && (ch = getchar()) != EOF; ++idx) {
+    s[idx] = ch;
+  }
+
+  s[idx] = '\0';
+  return idx;
 }
 
 /*
@@ -21,6 +39,7 @@ int strIndex(char s[], char t[]) {
 
   for (i = 0; s[i] != '\0'; ++i) {
     found = 0;
+    
     for (j = 0; t[j] != '\0'; ++j) {
       if (s[i + j] != t[j]) {
         found = -1;
