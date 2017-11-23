70ba52f5902503ed8e5c837db24bbcde9a717fd3
pbhaskal <pbhaskal@gmail.com>
2017-11-23 21:39:52 +0530

    getop is now handling signed decimal numbers.

--- cpractice/basicc/getop.c
+++ cpractice/basicc/getop.c
@@ -10,21 +10,38 @@ void myungetch(char);
   reads space/tab seperated operands and operators from input.
   skips spaces, tabs
 
-  does not yet handle positive/negative number, decimal number.
+  it does now handle positive/negative number.
 
   returns '0' when number is read.
   else returns the character read.
+  if newline is read, return it like other character. it may be used 
+  as special signal.
 */
 int getop(char s[]) {
   int idx = 0;
   int ch;
+  int afterSignCh;
 
   // skip spaces, tabs
   while ( (ch=mygetch()) == ' ' || ch == '\t')
     ;
 
   if (!isdigit(ch) && ch != '.') {
-    return ch;
+    // check if we have a signed number.
+    if (ch == '+' || ch == '-') { 
+      afterSignCh = mygetch();
+      if (isdigit(afterSignCh) || afterSignCh == '.') {
+        s[idx++] = ch;
+        ch = afterSignCh;
+      }
+      else {
+        myungetch(afterSignCh);
+        return ch;
+      }
+
+    }
+    else // for /, * operator
+      return ch;
   }
 
 // read decimal part.
