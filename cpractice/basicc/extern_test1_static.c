03994beac46758d07dedda148e2234260164834c
pbhaskal <pbhaskal@gmail.com>
2017-12-07 23:50:02 +0530

    trying register variable but it does not seem to be working.

--- cpractice/basicc/extern_test1_static.c
+++ cpractice/basicc/extern_test1_static.c
@@ -2,16 +2,19 @@
 
 int vara = 1.0;
 
-void test1();
+void test1(register int);
 void test2();
 
 
 int main() {
-  test1();
+  test1(6);
   test2();
 }
 
-void test1() {
+void test1(register int registerVar) {
   printf("printing from test1 - vara value %d\n", vara);
+  register long x = 1000000000;
+  while(x-- > 0)
+    ;
   return;
 }
